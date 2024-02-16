832c8f7f3620048e9b5b92dbc890ffe32b4c2c6d
Oldrado Junior <oldradojunior@gmail.com>
2024-02-15 21:04:34 -0300

    feat(frontend): add config for api requests

---
+++ frontend/src/utils/config.js
@@ -0,0 +1,33 @@
+export const api = import.meta.env.VITE_API_ENDPOINT || '';
+export const uploads = import.meta.env.VITE_UPLOAD_ENDPOINT || '';
+
+export const requestConfig = (method, data, token = null, image = null) => {
+  let config;
+
+  if (image) {
+    config = {
+      method,
+      body: data,
+      headers: {},
+    };
+  } else if (method === 'DELETE' || !data) {
+    config = {
+      method,
+      headers: {},
+    };
+  } else {
+    config = {
+      method,
+      body: JSON.stringify(data),
+      headers: {
+        'Content-Type': 'application/json',
+      },
+    };
+  }
+
+  if (token) {
+    config.headers.Authorization = `Bearer ${token}`;
+  }
+
+  return config;
+};
