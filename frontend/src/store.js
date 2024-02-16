88ddee329bc43ffcbbd4b7f25da8c6af76c3082d
Oldrado Junior <oldradojunior@gmail.com>
2024-02-16 02:24:12 -0300

    feat(frontend): add auth slice

--- frontend/src/store.js
+++ frontend/src/store.js
@@ -1,5 +1,9 @@
 import { configureStore } from '@reduxjs/toolkit';
 
+import authReducer from './slices/authSlice';
+
 export default configureStore({
-  reducer: {},
+  reducer: {
+    auth: authReducer,
+  },
 });
