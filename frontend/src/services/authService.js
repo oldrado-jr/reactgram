0a5e04b8caccb1b57709584606e6dcafe8a69960
Oldrado Junior <oldradojunior@gmail.com>
2024-02-15 21:05:21 -0300

    feat(frontend): add auth service

---
+++ frontend/src/services/authService.js
@@ -0,0 +1,23 @@
+import { api, requestConfig } from '../utils/config';
+
+// Register an user
+const register = async (data) => {
+  const config = requestConfig('POST', data);
+
+  try {
+    const response = await fetch(`${api}/users/register`, config);
+    const jsonResponse = await response.json();
+
+    if (jsonResponse) {
+      localStorage.setItem('user', JSON.stringify(jsonResponse));
+    }
+  } catch (error) {
+    console.error(error);
+  }
+};
+
+const authService = {
+  register,
+};
+
+export default authService;
