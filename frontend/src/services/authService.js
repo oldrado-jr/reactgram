0ca1d04d1197e1f12c3577f855d18650fde8e2c3
Oldrado Junior <oldradojunior@gmail.com>
2024-02-17 01:49:57 -0300

    feat(frontend): add login

--- frontend/src/services/authService.js
+++ frontend/src/services/authService.js
@@ -9,7 +9,7 @@ const register = async (data) => {
       .then((response) => response.json())
       .catch((err) => err);
 
-    if (response) {
+    if (response && !response.errors) {
       localStorage.setItem('user', JSON.stringify(response));
     }
 
@@ -24,9 +24,29 @@ const logout = () => {
   localStorage.removeItem('user');
 };
 
+// Sign in an user
+const login = async (data) => {
+  const config = requestConfig('POST', data);
+
+  try {
+    const response = await fetch(`${api}/users/login`, config)
+      .then((response) => response.json())
+      .catch((err) => err);
+
+    if (response && !response.errors) {
+      localStorage.setItem('user', JSON.stringify(response));
+    }
+
+    return response;
+  } catch (error) {
+    console.error(error);
+  }
+};
+
 const authService = {
   register,
   logout,
+  login,
 };
 
 export default authService;
